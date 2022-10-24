fe23b521b106b67827b17beaffca62b035895377
Yana Sirina <yanasirina0@yandex.ru>
2022-10-24 21:01:35 +0500

    Создала модель с информацией о пользователе

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class UserData(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    phone_number = models.CharField(verbose_name='Номер телефона', max_length=15)
+    city = models.CharField(verbose_name='Город', max_length=30)
+    street = models.CharField(verbose_name='Улица', max_length=50)
+    house = models.IntegerField(verbose_name='Номер дома')
+    apartment = models.IntegerField(verbose_name='Номер квартиры', blank=True, null=True)
