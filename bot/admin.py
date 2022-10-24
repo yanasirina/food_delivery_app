d2a33f903124d6cba9b10901199f8466f52f9c91
Yana Sirina <yanasirina0@yandex.ru>
2022-10-25 00:05:44 +0500

    Добавила модель курьеров

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Courier)
+class Courier(admin.ModelAdmin):
+    list_display = ('telegram_id', 'name', 'phone_number')
+    search_fields = ('telegram_id', 'name')
--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from core.models import Order
 
-# Create your models here.
+
+class Courier(models.Model):
+    telegram_id = models.IntegerField(verbose_name='ID чата курьера с ботом в телеграме')
+    name = models.CharField(verbose_name='Имя курьера', max_length=255)
+    phone_number = models.CharField(verbose_name='Номер телефона', max_length=15)
+    is_able = models.BooleanField(verbose_name='Доступен к заказам', default=False)
+    is_waiting_from = models.DateTimeField(verbose_name='Время ожидания нового заказа',
+                                           blank=True, null=True, default=None)
+    order = models.ForeignKey(Order, verbose_name='Детали заказа',
+                              blank=True, null=True, default=None, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = "Курьер"
+        verbose_name_plural = "Курьеры"
+
+    def __str__(self):
+        return f'{self.telegram_id}: {self.name}'
