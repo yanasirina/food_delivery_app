7a8d7435fb2b84161399a41fa1da3797ffdaa55d
Yana Sirina <yanasirina0@yandex.ru>
2022-10-24 23:08:54 +0500

    Добавила модели категорий, товаров и заказов

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class Category(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+
+
+@admin.register(models.Item)
+class Item(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+    autocomplete_fields = ('category', )
+
+
+@admin.register(models.Order)
+class Order(admin.ModelAdmin):
+    list_display = ('user', 'date', 'is_ordered', 'is_delivered')
+    autocomplete_fields = ('user', 'items')
+    list_editable = ('is_ordered', 'is_delivered')
+    list_filter = ('is_ordered', 'is_delivered', 'date')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Категория', max_length=100)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Список Категорий"
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.PROTECT)
+    name = models.CharField(verbose_name='Название товара', max_length=255)
+    description = models.TextField(verbose_name='Описание товара', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Список Товаров"
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item, verbose_name='Товары')
+    user = models.ForeignKey(User, verbose_name='Покупатель', on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name="Изменено", auto_now=True)
+    is_ordered = models.BooleanField(verbose_name='Заказ оформлен', default=False)
+    is_delivered = models.BooleanField(verbose_name='Заказ доставлен', default=False)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+    def __str__(self):
+        return f'{str(self.user)}: {self.date}'
