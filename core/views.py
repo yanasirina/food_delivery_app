67c1d2a06779462c17910cba8f20853698e16947
Yana Sirina <yanasirina0@yandex.ru>
2022-10-26 15:32:38 +0500

    Добавила возможность изменения поля in_stock

--- core/views.py
+++ core/views.py
@@ -32,10 +32,30 @@ class ItemViewSet(ReadOnlyModelViewSet):
     queryset = models.Item.objects.all()
     serializer_class = serializers.ItemGetSerializer
 
+    def get_queryset(self):
+        return models.Item.objects.filter(in_stock=True)
 
-class AdminItemViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin,
-                       mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    permission_classes = (IsAdminUser,)
+
+class AdminItemViewSet(ModelViewSet):
+    permission_classes = (IsAdminUser, )
     queryset = models.Item.objects.all()
     serializer_class = serializers.ItemSerializer
+
+    @action(detail=True, methods=['patch'])
+    def set_in_stock(self, request, pk):
+        item = self.get_object()
+        if not item.in_stock:
+            item.in_stock = True
+            item.save(update_fields=['in_stock'])
+        serializer = serializers.ItemSerializer(instance=item)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['patch'])
+    def unset_in_stock(self, request, pk):
+        item = self.get_object()
+        if item.in_stock:
+            item.in_stock = False
+            item.save(update_fields=['in_stock'])
+        serializer = serializers.ItemSerializer(instance=item)
+        return Response(serializer.data)
 
