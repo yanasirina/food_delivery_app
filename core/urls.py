4002083671c63805787a664c4bb0722a45223de4
Yana Sirina <yanasirina0@yandex.ru>
2022-10-26 14:44:04 +0500

    Прописала CRUD для категорий

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from . import views
 
 urlpatterns = [
-
+    path('create_category/', views.CreateCategory.as_view(), name='create_category'),
+    path('cat/<int:pk>/', views.UpdateDeleteCategory.as_view(), name='cat'),
 ]
+
+router = DefaultRouter()
+router.register('categories', views.CategoryViewSet, basename='category')
+urlpatterns += router.urls
--- core/views.py
+++ core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import mixins, generics, viewsets
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from . import serializers
+from . import models
+
+
+class CategoryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def items(self, request, pk):
+        category = models.Category.objects.get(pk=pk)
+        items = models.Item.objects.filter(category=category)
+        items = [str(c) for c in items]
+        return Response({'Товары': items})
+
+
+class UpdateDeleteCategory(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAdminUser, )
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class CreateCategory(generics.CreateAPIView):
+    permission_classes = (IsAdminUser, )
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
